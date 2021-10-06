916140b75f230a50f114156a4ac6fc34bfb7898a
Chris Vickery <vickeryc@newpaltz.edu>
2021-10-06 16:36:24 -0400

    More string handling and our first segfault

--- learning.c
+++ learning.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 #include<string.h>
 
+int compstr(char *a, char *b) {
+    int i;
+    while(a[i] != '\0' && b[i] != '\0') {
+        if(a[i] != b[i]) {
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
+
 int main() {
 //    for (int i=0; i<10; i++) {
 //        printf("%d\n",i);
@@ -14,18 +25,26 @@ int main() {
     printf("%d\n", length);
 
     fgets(word2, 25, stdin);
+    int x = 0;
+    for (int y = 0; y < length; y++) {
+        if (word[y] == word2[y] ) {
+            x++;
+        } else {
+            continue;
+        }
+    }
 
-    if (strcmp(word,word2) == 0) {  // strcmp returns 0 if strings match
+    if (compstr(word,word2) == 0) {  // strcmp returns 0 if strings match
         printf("It matches!\n");
     } else {
         printf("It does not matches!\n");
     }
 
-    for (int i = 0; i < length; i++) {  // iterate through word and print characters
-        printf("%c\n",word[i]);
-    }
+//    for (int i = 0; i < length; i++) {  // iterate through word and print characters
+//        printf("%c\n",word[i]);
+//    }
 //    fgets(word, 25, stdin);  // get user input, ignore spaces and such
 //    printf("Your name is: %s", word);
-
+    printf("There are %d matching letters!\n", x);
     return 0;
 }
