4385740c5c70bdceb16f320746045fd573919fd9
Chris Vickery <vickeryc@newpaltz.edu>
2021-10-06 12:48:41 -0400

    Learning about strings

--- learning.c
+++ learning.c
@@ -1,6 +1,22 @@
 #include<stdio.h>
+#include<string.h>
 
 int main() {
-    printf("Howdy\n");
+//    for (int i=0; i<10; i++) {
+//        printf("%d\n",i);
+//    }
+
+    char word[] = "Sandwich"; // word variable
+    int length = strlen(word);  // get word length
+
+    printf("%s\n", word);
+    printf("%d\n", length);
+
+    for (int i = 0; i < length; i++) {  // iterate through word and print characters
+        printf("%c\n",word[i]);
+    }
+//    fgets(word, 25, stdin);  // get user input, ignore spaces and such
+//    printf("Your name is: %s", word);
+
     return 0;
 }
