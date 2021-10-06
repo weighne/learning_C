71f9983c07adccfb888c8f4963615604a3e10c71
Chris Vickery <vickeryc@newpaltz.edu>
2021-10-06 14:23:26 -0400

    String comparisons

--- learning.c
+++ learning.c
@@ -8,9 +8,18 @@ int main() {
 
     char word[] = "Sandwich"; // word variable
     int length = strlen(word);  // get word length
+    char word2[25];
 
     printf("%s\n", word);
     printf("%d\n", length);
+
+    fgets(word2, 25, stdin);
+
+    if (strcmp(word,word2) == 0) {  // strcmp returns 0 if strings match
+        printf("It matches!\n");
+    } else {
+        printf("It does not matches!\n");
+    }
 
     for (int i = 0; i < length; i++) {  // iterate through word and print characters
         printf("%c\n",word[i]);
